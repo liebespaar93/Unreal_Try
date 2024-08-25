83206cd865c30c2bd57cb2275f8839d3151b0a80
아기새 <liebespaar93@gmail.com>
2024-08-25 13:43:13 +0900

    add Emeny (#2)

--- Source/Unreal_Try/Private/CPP_TPSEnemy.cpp
+++ Source/Unreal_Try/Private/CPP_TPSEnemy.cpp
@@ -9,6 +9,17 @@ ACPP_TPSEnemy::ACPP_TPSEnemy()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Load Character Mesh
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempCharacterSkeletalMesh(TEXT("/ Script / Engine.SkeletalMesh'/Game/Kyoulee/BluePrints/Enemy/Ch32_nonPBR_UE.Ch32_nonPBR_UE'"));
+
+	if (tempCharacterSkeletalMesh.Succeeded()) {
+		this->uCharacterSkeletalMesh = tempCharacterSkeletalMesh.Object;
+		this->GetMesh()->SetSkeletalMesh(this->uCharacterSkeletalMesh);
+		this->GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
+		this->GetMesh()->SetRelativeLocation(FVector(0, 0, -90));
+	}
+
+	
 }
 
 // Called when the game starts or when spawned
